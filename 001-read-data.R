e46889c58c4b04c87baec1872ae07017e0c837de
luehkenecology <luehken65@gmail.com>
2018-03-16 13:58:08 +0100

    change names of titles

--- 001-read-data.R
+++ 001-read-data.R
@@ -9,21 +9,21 @@
 #library(ggplot2)
 #library(scales)
 
-# Pathogen models----------------------------------------------------------------------
-# Bluetongue virus
+# pathogen models----------------------------------------------------------------------
+# bluetongue virus
 btv <- function(Temp){0.0003*Temp*(Temp-10.4057)/24}
 
-# Schmallenberg virus
+# schmallenberg virus
 sbv <- function(Temp){(0.019*(Temp-13.3))/24}
 
-# Dengue virus
+# dengue virus
 denv <- function(Temp){(-0.1393+0.008*Temp)/24}
 
-# Dirofilaria immitis
+# dirofilaria immitis
 diro <- function(Temp){(Temp-14/130)/24}
 
-# Malaria
+# malaria
 malaria <- function(Temp){(0.000126*Temp+(Temp-14.244)*sqrt(34.4-Temp))/24}
 
-# West Nile virus
+# west nile virus
 wnv <- function(Temp){(-0.132+0.0092*Temp)/24}
